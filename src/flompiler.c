7a540d1ab363aa9c8d7279e3335b46cc3a70b821
otdav33 <odavies@dread.life>
2017-04-10 20:38:13 -0400

    Made one file

--- src/flompiler.c
+++ src/flompiler.c
@@ -1,9 +1,32 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include"flompiler.h"
 
+
+#define MAXVALS 8 //number of inputs/outputs per function max
+#define WORDLEN 100 //largest word size
+#define LINELEN 5000 //largest line size
+#define MAXLINES 5000 //largest line size
+#define MAXSCOPES 100 //largest line size
+#define MAXWORDS 17 //most words per line
+
+#define eprint(expr) fprintf(stderr, expr);
 #define dprint(expr, f) printf(#expr " = '%" f "'\n", expr); //for debugging
+
+struct func {
+	char ins[MAXVALS][WORDLEN], outs[MAXVALS][WORDLEN], name[WORDLEN]; //inputs and outputs, name of function
+	//terminators are if (ins[i][0]) (outs[i][0]) (name[0]) then end
+	char satisfied; //mask for if the inputs are satisfied
+};
+
+struct scope {
+	char name[WORDLEN];
+	struct func f[MAXLINES];
+	int subscopes;
+};
+
+//update every func.satisfied flag for a value
+void satisfy(char *program, struct func *funcs, char *value);
 
 //TODO: support multiple return values
 
