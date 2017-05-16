8cfe00dacebf4f91cffd6477d429f51894733438
otdav33 <odavies@dread.life>
2017-05-15 21:18:56 -0400

    started to make runfunc

--- src/flompiler.py
+++ src/flompiler.py
@@ -1,6 +1,7 @@
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
 import sys
+import copy
 
 flprog = sys.stdin.read() #take input from stdin
 output = "" #final thing to print out
@@ -53,4 +54,46 @@ def parse(string):
 
 scopes = parse(flprog)
 
-print(scopes)
+def runline(line, scope):
+    retval = ""
+    if line.function[0] == "'":
+        for o in line.outputs:
+            retval += o + " = '" + line.function[1:] + "';\n"
+            retval += satisfy(o)
+    elif line.function[0] == "#":
+        for o in line.outputs:
+            retval += o + " = " + line.function[1:] + ";\n"
+            retval += satisfy(o)
+    elif line.function == "<":
+        for o in line.outputs:
+            retval += o + " = " + line.inputs[0] + ";\n"
+            retval += satisfy(o)
+    elif line.function == ">":
+        retval += "if (" + line.inputs[0] + " > " + line.inputs[2] + ") {\n"
+        temp = copy.deepcopy(scope) #copy scope
+        retval += line.outputs[0] + " = " + line.inputs[1] + ";\n"
+        retval += satisfy(line.outputs[0])
+        retval += "} else {\n"
+        temp = copy.deepcopy(scope) #copy scope again
+        retval += line.outputs[1] + " = " + line.inputs[1] + ";\n"
+        retval += satisfy(line.outputs[1])
+        retval += "}\n"
+    else:
+        for operator in "+-*/%":
+            if operator == line.function:
+                for o in line.outputs:
+                    retval += o + " = "
+                    for i in line.inputs:
+                        retval += i + " " + line.function + " "
+                    retval = retval[:-2]
+                    retval += ";\n"
+                break
+        else:
+            retval += line.function + "("
+            args = ""
+            for o in line.outputs:
+                args += "&" + o + ", "
+            for i in line.inputs:
+                args += i + ", "
+            args = args[:-2]
+            retval += args + ");\n"
